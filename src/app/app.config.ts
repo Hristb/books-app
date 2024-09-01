aadafdc27312b9071d5c8b49933e581d91a1ef88
Hrist Joy <hristbartra@gmail.com>
2024-09-01 00:30:40 -0500

    Implement custom preloading and header

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withPreloading } from '@angular/router';
 
 import { routes } from './app.routes';
+import { MyPreloadingStrategy } from './core/strategies/my-preloading-strategy';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    MyPreloadingStrategy,
+    provideRouter(routes,withPreloading(MyPreloadingStrategy)),
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'Home'
+    },
+    {
+        path: 'Home',
+        pathMatch:'full',
+        loadComponent:() => import('./public/home/home.component').then((m)=> m.HomeComponent),
+    }
+];
